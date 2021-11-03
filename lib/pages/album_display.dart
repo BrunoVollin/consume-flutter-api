3daf398e816d2c91bcf85ab6854bcee722d3108d
Bruno <bruno.3av@gmail.com>
2021-11-03 08:53:21 -0300

    album display file added

---
+++ lib/pages/album_display.dart
@@ -0,0 +1,86 @@
+import 'package:flutter/material.dart';
+import 'package:test/repositories/album_repository.dart';
+import 'package:test/model/album_model.dart';
+
+class AlbumDisplay extends StatefulWidget {
+  AlbumDisplay({Key? key, required this.album}) : super(key: key);
+  final Album album;
+
+  @override
+  _AlbumDisplayState createState() => _AlbumDisplayState();
+}
+
+class _AlbumDisplayState extends State<AlbumDisplay> {
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.all(8.0),
+      child: Card(
+        child: Padding(
+          padding: const EdgeInsets.all(20.0),
+          child: Column(
+            children: [
+              Row(
+                children: [
+                  Container(
+                    // width: double.infinity,
+                    child: const Text(
+                      "Title: ",
+                      textAlign: TextAlign.left,
+                      style: TextStyle(fontSize: 20, color: Colors.blue),
+                      softWrap: false,
+                      overflow: TextOverflow.fade,
+                    ),
+                  ),
+                  Flexible(
+                    fit: FlexFit.loose,
+                    child: Text(
+                      widget.album.title,
+                      // textAlign: TextAlign.center,
+                      softWrap: false,
+                      overflow: TextOverflow.ellipsis,
+                      // textAlign: TextAlign.right,
+                      style: const TextStyle(fontSize: 25),
+                    ),
+                  ),
+                ],
+              ),
+              Row(
+                children: [
+                  Container(
+                    // width: double.infinity,
+                    child: const Text(
+                      "Id do Usuario: ",
+                      textAlign: TextAlign.left,
+                      style: TextStyle(fontSize: 20, color: Colors.blue),
+                    ),
+                  ),
+                  Text(
+                    "${widget.album.userId}",
+                    style: const TextStyle(fontSize: 20),
+                  ),
+                ],
+              ),
+              Row(
+                children: [
+                  Container(
+                    // width: double.infinity,
+                    child: const Text(
+                      "Id do Album: ",
+                      textAlign: TextAlign.left,
+                      style: TextStyle(fontSize: 20, color: Colors.blue),
+                    ),
+                  ),
+                  Text(
+                    "${widget.album.userId}",
+                    style: const TextStyle(fontSize: 20),
+                  ),
+                ],
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
